501ab3dfe323b95c58844f35d95309460887986f
JhoanSLC <JhoanSLC4@gmail.com>
2024-09-26 10:46:58 -0500

    :tada: Inicio de proyecto :tada:

---
+++ src/main/java/com/tiendacrud/tiendacrud/TiendacrudApplication.java
@@ -0,0 +1,13 @@
+package com.tiendacrud.tiendacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendacrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendacrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendacrud/tiendacrud/TiendacrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendacrud.tiendacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendacrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
